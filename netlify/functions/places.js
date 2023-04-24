9323a5255b1861676626596b4ee74aa7560d1cae
Adrian Trilling <adrian.trilling@gmail.com>
2023-04-23 22:56:59 -0400

    10:56

--- netlify/functions/places.js
+++ netlify/functions/places.js
@@ -1,9 +1,6 @@
 const api_url = "https://developer.nps.gov/api/v1/places"
 
-const params = new URLSearchParams({
-    limit: 10,
-    api_key: process.env.NPS_API_KEY
-})
+
 
 // video 12, 1:24:09
 
@@ -11,6 +8,11 @@ exports.handler = async (event, context) => {
     const park_code = event.queryStringParameters.park_code
     // http://localhost:8888/.netlify/functions/hello?park_code=[park_code]
 
+    const params = new URLSearchParams({
+        limit: 10,
+        api_key: process.env.NPS_API_KEY,
+        park_code: park_code
+    })
     try {
         const response = await fetch(`${api_url}?${params}`, {
             headers: {
