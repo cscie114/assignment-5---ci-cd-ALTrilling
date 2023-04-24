0c0e458caf85d6f5fea4719e4af264f699de6b64
Adrian Trilling <adrian.trilling@gmail.com>
2023-04-23 22:31:06 -0400

    10:25

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,6 @@
 export const handler = async (event, context) => {
-    const park_code = event.queryStringParameters.park_code
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: "Hello World", park_code: park_code })
-        }
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello World" })
+    }
 }
