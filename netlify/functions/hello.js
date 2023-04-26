7265d0f55681bce90a7d32f6409d51843664c773
Adrian Trilling <adrian.trilling@gmail.com>
2023-04-25 22:24:39 -0400

    10:24

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,7 @@
 const handler = async function (event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Functions are working." })
+        body: JSON.stringify({ message: "Functions are working. test" })
     };
 };
 
